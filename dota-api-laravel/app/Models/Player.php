80e9f1162864201718c9c4038434d09331fc1e61
Sergey <3112joker3112@gmail.com>
2023-12-21 15:56:03 +0400

    add Player and User models

---
+++ dota-api-laravel/app/Models/Player.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class Player extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id',
+        'name',
+    ];
+
+    function user(): MorphTo
+    {
+        return $this->morphTo();
+    }
+}
