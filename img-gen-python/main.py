ab6b5391531bade39652861ec65e38c5e68199b6
Александр Матвеев <matveevalex97@yandex.ru>
2024-02-17 18:57:17 +0300

    susya-pussya

---
+++ img-gen-python/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from html2image import Html2Image
+
+
+app = FastAPI()
+
+@app.api_route("/get_html/", methods=['POST'])
+async def get_html(request: Request):
+    body = await request.json()
+    html = body.get('html')
+    name = body.get('name')
+    hti = Html2Image(output_path='/media/generated_images/')
+    hti.screenshot(html_str=html, save_as=f'{name}.png')
+    return JSONResponse({})
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run('main:app', host="0.0.0.0", port=8090, loop="asyncio", reload=True)
